11be6b94ac89136e81bf80269d15d3eb444412bd
EpicDeveloper <vgdobon91@gmail.com>
2020-07-22 17:19:52 +0200

    Movimiento pelota(en desarrollo)

---
+++ src/com/hiberus/Main.java
@@ -0,0 +1,9 @@
+package com.hiberus;
+
+//. Pelota [PENDIENTE]
+//Crear una clase Pelota que tenga:
+//- Constructor (x: int, y: int)
+//- getters y setters
+//- move(speed: int, direction: int) (direction serán los grados entre 0-360)
+public class Main {
+}
---
+++ src/com/hiberus/Pelota.java
@@ -0,0 +1,10 @@
+package com.hiberus;
+
+public class Pelota {
+    private int x;
+    private int y;
+
+    public Pelota(){
+        this.x=x;
+    }
+}
